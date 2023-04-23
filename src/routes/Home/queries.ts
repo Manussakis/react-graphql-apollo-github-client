ee70e4a069f98c2cc411d1427e54e284087341a0
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2023-04-23 15:40:48 -0300

    Add id to Organization query

--- src/routes/Home/queries.ts
+++ src/routes/Home/queries.ts
@@ -2,8 +2,9 @@ import { gql } from "@apollo/client";
 import { REPOSITORY_FRAGMENT } from "../../components/Repository";
 
 export const GET_REPOSITORIES_OF_ORGANIZATION = gql`
-  query ($organizationName: String!, $cursor: String) {
+  query RepositoriesOfOrganizations ($organizationName: String!, $cursor: String) {
     organization(login: $organizationName) {
+      id
       repositories(first: 5, after: $cursor) {
         edges {
           node {
