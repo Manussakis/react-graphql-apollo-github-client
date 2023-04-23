a004b1f4cad01f2deb908e6ad36981dac2a4bd7e
Gabriel Manussakis <gabrielmanussakis@gmail.com>
2023-04-23 15:42:48 -0300

    Clean and format App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,30 @@
-import { useEffect, useState } from 'react';
-import { useQuery, gql } from '@apollo/client';
-import { Outlet } from 'react-router-dom'
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-import './App.scss';
-
-const GET_GITHUB_USER = gql`
-  query {
-    viewer {
-      id
-      login
-      name
-      repositories(first: 5 orderBy: {direction: DESC, field: STARGAZERS}) {
-        edges {
-          node {
-            name
-          }
-        }
-      }
-    }
-  }
-`;
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import "./App.scss";
 
 function App() {
-  const [organizationName, setOrganizationName] = useState('facebook');
-  
+  const [organizationName, setOrganizationName] = useState("facebook");
+
   const onOrganizationSearch = (value: string) => {
     setOrganizationName(value);
   };
 
   return (
     <div className="App">
-      <Navigation organizationName={organizationName} onOrganizationSearch={onOrganizationSearch} />
+      <Navigation
+        organizationName={organizationName}
+        onOrganizationSearch={onOrganizationSearch}
+      />
       <h1>Github Client</h1>
       <div className="App-content_small-header">
         <Outlet context={organizationName} />
       </div>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
